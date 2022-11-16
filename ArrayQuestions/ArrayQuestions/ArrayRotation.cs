7f8d76fd763c5a164d4fe9df961c674096ddbd80
Nikhil Personal Laptop <imnikhilkumar78@gmail.com>
2022-11-16 14:05:34 +0530

    Rotate Array Operations in Array Questions

---
+++ ArrayQuestions/ArrayQuestions/ArrayRotation.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayQuestions
+{
+    public class ArrayRotation
+    {
+        public void RotateArray()
+        {
+            Console.WriteLine("Enter Size Of Array To be Rotated");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] originalArray = new int[n];
+            Console.WriteLine("Enter the Original Array");
+            for(int i=0;i<n;i++)
+            {
+                originalArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the value you want by which you want array to rotate");
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1. Enter 1 to rotate Left" +
+                "2. Enter 2 to rotate Right");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            int[] rotatedArray=new int[n];
+
+            switch(choice)
+            {
+                case 1:
+                    rotatedArray=printLeftRotatedArray(originalArray, d, n);
+                    break;
+                case 2:
+                    rotatedArray=printRightRotatedArray(originalArray, d, n);
+                    break;
+            }
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Original Array");
+            Console.WriteLine("-------------------------------------------------");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(originalArray[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Rotated Array");
+            Console.WriteLine("--------------------------------------------------");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(rotatedArray[i]);
+
+            }
+
+
+
+        }
+
+        public int[] printLeftRotatedArray(int[] originalArray,int d, int n)
+        {
+            int k = 0;//Keeping rotated array index
+
+            int[] rotatedarray=new int[n];
+            for(int i=d;i<n;i++)
+            {
+                rotatedarray[k]=originalArray[i];
+                k++;
+            }
+            for (int i = 0; i < d; i++)
+            {
+                rotatedarray[k] = originalArray[i];
+                k++;
+            }
+            return rotatedarray;
+            
+        }
+        public int[] printRightRotatedArray(int[] originalArray,int d, int n)
+        {
+            int k = 0;//Keeping rotated array index
+
+            int[] rotatedarray = new int[n];
+            for (int i = n-d; i < n; i++)
+            {
+                rotatedarray[k] = originalArray[i];
+                k++;
+            }
+            for (int i = 0; i < n-d; i++)
+            {
+                rotatedarray[k] = originalArray[i];
+                k++;
+            }
+            return rotatedarray;
+
+        }
+    }
+}
---
+++ ArrayQuestions/ArrayQuestions/Program.cs
@@ -0,0 +1,38 @@
+﻿using ArrayQuestions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Program prg = new Program();
+        prg.printMenu();
+
+        Console.ReadKey();
+
+        
+    }
+
+    public void printMenu()
+    {
+        Program prg = new Program();
+        Console.WriteLine("Enter Number of Operation You want to perform");
+        Console.WriteLine("1. Rotate Array by D");
+        try
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("Rotation Of Array");
+                    ArrayRotation arrayRotation = new ArrayRotation();
+                    arrayRotation.RotateArray();
+                    break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Enter only Integer from Menu : "+e.Message);
+            prg.printMenu();
+        }
+    }
+}
